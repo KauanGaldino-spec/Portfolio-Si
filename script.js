08c1a907ea830be7f91137f83f5a8b6de565fcf7
KauanGaldino-spec <kauangaldino133@gmail.com>
2025-02-17 17:25:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+const sideMenu = document.querySelector("#sideMenu");
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
+
+// Abre o menu móvel
+function openMenu() {
+  if (sideMenu) {
+    sideMenu.style.transform = "translateX(0)";
+  }
+}
+
+function closeMenu() {
+  if (sideMenu) {
+    sideMenu.style.transform = "translateX(100%)";
+  }
+}
+
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+  } else {
+    navBar.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+  }
+});
+
+// Fecha o menu móvel ao clicar em um link
+const sideMenuLinks = document.querySelectorAll("#sideMenu a");
+if (sideMenuLinks.length > 0) {
+  sideMenuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      closeMenu();
+    });
+  });
+}
+
+// Script para scroll suave
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    !sideMenu.contains(e.target) &&
+    !e.target.closest(".menu-toggle-button")
+  ) {
+    closeMenu();
+  }
+});
+
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+);
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+}
