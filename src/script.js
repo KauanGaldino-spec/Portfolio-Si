1a5324ec3da810f8e2b792d9d2cd016eb1e39f29
KauanGaldino-spec <kauangaldino133@gmail.com>
2025-02-17 17:30:25 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -2,25 +2,27 @@ const sideMenu = document.querySelector("#sideMenu");
 const navBar = document.querySelector("nav");
 const navLinks = document.querySelector("nav ul");
 
-// Funções para abrir e fechar o menu móvel
+// Abre o menu móvel
 function openMenu() {
-  sideMenu.style.transform = "translateX(0)";
+  if (sideMenu) {
+    sideMenu.style.transform = "translateX(0)";
+  }
 }
 
 function closeMenu() {
-  sideMenu.style.transform = "translateX(100%)";
+  if (sideMenu) {
+    sideMenu.style.transform = "translateX(100%)";
+  }
 }
 
-// Adiciona o evento de scroll para mudar o estilo da barra de navegação
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
+  if (scrollY > 50) {
     navBar.classList.add(
       "bg-white",
       "bg-opacity-50",
       "backdrop-blur-lg",
       "shadow-sm"
     );
-    navLinks.classList.remove("bg-white", "shadow-sm", "bg-opacity-50");
   } else {
     navBar.classList.remove(
       "bg-white",
@@ -28,32 +30,53 @@ window.addEventListener("scroll", () => {
       "backdrop-blur-lg",
       "shadow-sm"
     );
-    navLinks.classList.add("bg-white", "shadow-sm", "bg-opacity-50");
   }
 });
 
 // Fecha o menu móvel ao clicar em um link
-document.querySelectorAll("#sideMenu a").forEach((link) => {
-  link.addEventListener("click", () => {
-    closeMenu();
+const sideMenuLinks = document.querySelectorAll("#sideMenu a");
+if (sideMenuLinks.length > 0) {
+  sideMenuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      closeMenu();
+    });
   });
-});
+}
 
-// Adiciona scroll suave para todos os links âncora
+// Script para scroll suave
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
-    e.preventDefault(); // Evita o comportamento padrão do link
-    const targetId = this.getAttribute("href"); // Pega o ID do alvo
-    const targetElement = document.querySelector(targetId); // Seleciona o elemento alvo
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: "smooth", block: "start" }); // Rola suavemente até o alvo
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
     }
   });
 });
 
-// Fecha o menu ao clicar fora dele
 document.addEventListener("click", (e) => {
-  if (!sideMenu.contains(e.target) && !e.target.closest("button[onclick='openMenu()']")) {
+  if (
+    !sideMenu.contains(e.target) &&
+    !e.target.closest(".menu-toggle-button")
+  ) {
     closeMenu();
   }
 });
+
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+);
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+}
